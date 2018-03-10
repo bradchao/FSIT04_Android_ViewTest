19b0e93722a0d5d5ab2fd0da38c1445dde5f0dff
Brad <brad@brad.tw>
2018-03-10 16:38:52 +0800

    v5

--- app/src/main/java/tw/org/iii/viewtest/MyView2.java
+++ app/src/main/java/tw/org/iii/viewtest/MyView2.java
@@ -10,39 +10,64 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Created by Administrator on 2018/3/10.
  */
 
 public class MyView2 extends View {
+    private LinkedList<HashMap<String,Float>> line;
+    private Paint paint;
+
     public MyView2(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.YELLOW);
-        Log.v("brad", "MyView2()");
 
-        setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v("brad", "setOnClick");
-            }
-        });
+        line = new LinkedList<>();
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(4);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.v("brad", "MyView2:onDraw()");
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(4);
 
-        canvas.drawLine(0, 0,100,200, paint);
+        for (int i=1; i<line.size(); i++) {
+            HashMap<String,Float> p0 = line.get(i-1);
+            HashMap<String,Float> p1 = line.get(i);
+            canvas.drawLine(p0.get("x"), p0.get("y"),
+                    p1.get("x"), p1.get("y"), paint);
+        }
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Log.v("brad", "onTouch");
-        return super.onTouchEvent(event);
+        float ex = event.getX();
+        float ey = event.getY();
+
+        String action = null;
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            action = "Down";
+        }else if (event.getAction() == MotionEvent.ACTION_MOVE){
+            action = "Move";
+        }else if (event.getAction() == MotionEvent.ACTION_UP){
+            action = "UP";
+        }
+
+        if (action != null) {
+            Log.v("brad", action + ex + "x" + ey);
+
+            HashMap<String,Float> point = new HashMap<>();
+            point.put("x", ex); point.put("y", ey);
+            line.add(point);
+            invalidate();
+
+        }
+        return true; //super.onTouchEvent(event);
     }
 }
